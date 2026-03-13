7051590fd51da0d57ff0ab7ebe792dbd71fe4ecb
Isak Landin <utbisalan@gmail.com>
2026-03-13 15:27:13 +0100

    Moved errors to global lib

---
+++ errors.py
@@ -0,0 +1,29 @@
+class FileProcessingError(Exception):
+    def __init__(self, file_name=None, message=" FAILED during processing of file", code="FileProcessingError:"):
+        self.file_name = file_name
+        self.code = code
+        self.message = self.combine_parameters_to_message(message)
+        super().__init__(self)
+
+    def combine_parameters_to_message(self, _passed_message):
+        _original_message = self.message
+        _new_message = (
+                f"{_original_message}: " +
+                f"{self.file_name}\n: " if self.file_name else " " +
+                f"{self.code}"
+        )
+        return _new_message
+
+
+class GitHubError(Exception):
+    def __init__(self, message=" FAILED during processing of file", code="GitHubError:"):
+        self.passed_message = message
+        self.code = code
+        self.message = self.combine_parameters_to_message()
+        super().__init__(self)
+
+    def combine_parameters_to_message(self):
+        _new_message = (
+            f"{self.passed_message}: " + f"{self.code}"
+        )
+        return _new_message
