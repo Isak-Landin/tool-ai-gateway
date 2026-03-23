c1e060b54599ac6e78d70661dcb601d4216ef1a5
Isak Landin <utbisalan@gmail.com>
2026-03-23 06:55:42 +0100

    Added children of PersistenceError to better represent the using layer - clearer debug and error representation

--- errors.py
+++ errors.py
@@ -18,3 +18,34 @@ class GitHubError(Exception):
     def __init__(self, message: str):
         self.message = message
         super().__init__(message)
+
+
+class RoutePersistenceError(PersistenceError):
+    """Route-facing persistence error"""
+
+
+class ResolutionPersistenceError(PersistenceError):
+    """Project resolution persistence error"""
+
+
+class RuntimeBindingPersistenceError(PersistenceError):
+    """Runtime binding persistence error"""
+
+
+class ProjectHandlePersistenceError(PersistenceError):
+    """Project handle persistence error"""
+
+
+class ExecutionPersistenceError(PersistenceError):
+    """Execution persistence error"""
+    def __init__(self, message: str, field: str = None, error_type: str = None, file_id: str = None):
+        super().__init__(message=message, field=field, error_type=error_type)
+        self.file_id = file_id
+
+
+class MessageHistoryPersistenceError(PersistenceError):
+    """Message history persistence error"""
+
+
+class RepositoryFilePersistenceError(PersistenceError):
+    """Repository file persistence error"""
