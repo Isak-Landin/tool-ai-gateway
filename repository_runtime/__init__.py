2112e6238de62e0b9582040a35e258e924228bb0
Isak Landin <utbisalan@gmail.com>
2026-03-30 10:12:01 +0200

    Added exposed funtions to allow for new File responsible object to be able to fulfill its intended functionality

--- repository_runtime/__init__.py
+++ repository_runtime/__init__.py
@@ -1,7 +1,7 @@
 from repository_runtime.RepositoryRuntime import RepositoryRuntime
 from repository_runtime.bootstrap import ProjectBootstrap
 from repository_runtime.git import git_add, git_commit, git_pull, git_push, git_switch_branch, run_git
-from repository_runtime.inspection import list_repository_tree, search_repository_text
+from repository_runtime.inspection import list_repository_tree, read_repository_file, search_repository_text
 from repository_runtime.shell import ProjectShell, ProjectShellError
 
 __all__ = [
@@ -17,4 +17,5 @@ __all__ = [
     "git_commit",
     "search_repository_text",
     "list_repository_tree",
+    "read_repository_file",
 ]
--- repository_runtime/inspection/__init__.py
+++ repository_runtime/inspection/__init__.py
@@ -1,4 +1,5 @@
 from repository_runtime.inspection.list_tree import list_repository_tree
+from repository_runtime.inspection.read_file import read_repository_file
 from repository_runtime.inspection.search_text import search_repository_text
 
-__all__ = ["search_repository_text", "list_repository_tree"]
+__all__ = ["search_repository_text", "list_repository_tree", "read_repository_file"]
