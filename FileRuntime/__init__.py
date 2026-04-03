e9758ab99da2f9aa6723106c11f67ab315e0dadb
Isak Landin <utbisalan@gmail.com>
2026-04-03 11:51:10 +0200

    No longer an object, has been moved to act as encapsulated functions that acts on internal helpers to finalize tasks

--- FileRuntime/__init__.py
+++ FileRuntime/__init__.py
@@ -1,3 +1,17 @@
-from FileRuntime.FileRuntime import FileRuntime
+from FileRuntime.FileRuntime import (
+    get_ignore_patterns,
+    get_persisted_file,
+    list_persisted_files,
+    persist_file_snapshot,
+    read_file,
+    search_text,
+)
 
-__all__ = ["FileRuntime"]
+__all__ = [
+    "get_ignore_patterns",
+    "read_file",
+    "search_text",
+    "list_persisted_files",
+    "get_persisted_file",
+    "persist_file_snapshot",
+]
