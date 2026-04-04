a5ccb13427c892dca887724ae8f421d5f6443b19
Isak Landin <utbisalan@gmail.com>
2026-04-04 20:17:51 +0200

    Adapted to represent exports of current accurate representation

--- repository_runtime/__init__.py
+++ repository_runtime/__init__.py
@@ -1,6 +1,6 @@
 from repository_runtime.RepositoryRuntime import RepositoryRuntime
 from repository_runtime.bootstrap import bs1
-from repository_runtime.git import git_add, git_commit, git_pull, git_push, git_switch_branch, run_git
+from repository_runtime.git import git_add, git_commit, git_pull, git_push, git_switch_branch
 from repository_runtime.shell import ProjectShell, ProjectShellError
 
 __all__ = [
@@ -8,7 +8,6 @@ __all__ = [
     "ProjectShell",
     "ProjectShellError",
     "bs1",
-    "run_git",
     "git_pull",
     "git_switch_branch",
     "git_push",
